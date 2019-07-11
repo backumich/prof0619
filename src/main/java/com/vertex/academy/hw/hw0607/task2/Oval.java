07488eec5f005c43ef315e0b76e582dd91c93896
Roma <romsk94@gmail.com>
2019-07-11 09:57:13 +0300

    task2

---
+++ src/main/java/com/vertex/academy/hw/hw0607/task2/Oval.java
@@ -0,0 +1,14 @@
+package com.vertex.academy.hw.hw0607.task2;
+
+public class Oval {
+    private Point centre;
+    private int width;
+    private int height;
+
+
+    public Oval(Point centre, int width, int height) {
+        this.centre = centre;
+        this.width = width;
+        this.height = height;
+    }
+}
--- src/main/java/com/vertex/academy/hw/hw0607/task2/Point.java
+++ src/main/java/com/vertex/academy/hw/hw0607/task2/Point.java
@@ -1,5 +1,19 @@
 package com.vertex.academy.hw.hw0607.task2;
 
 public final class Point {
+    private final int axisX;
+    private final int axisY;
 
+    public Point(int axisX, int axisY ) {
+        this.axisX = axisX;
+        this.axisY = axisY;
+    }
+
+    public int getAsixX() {
+        return axisX;
+    }
+
+    public int getAxisY() {
+        return axisY;
+    }
 }
