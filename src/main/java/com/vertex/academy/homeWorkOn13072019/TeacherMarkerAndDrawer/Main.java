31b4baf9e713a6e4b41fe8397d9d30317ca0eb49
SynRay <rudik.nikita220899@gmail.com>
2019-07-12 13:56:33 +0300

    HomeWork on 12.07.2019

---
+++ src/main/java/com/vertex/academy/homeWorkOn13072019/TeacherMarkerAndDrawer/Main.java
@@ -0,0 +1,9 @@
+package com.vertex.academy.homeWorkOn13072019.TeacherMarkerAndDrawer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        Board.viewBoard();
+    }
+}
